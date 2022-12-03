de9ce66ed9e1b4001f508abb55a2ae4b8af7ee40
Aryaman R <aryamanrtunjay@gmail.com>
2022-12-03 15:58:41 -0800

    Update data_reader.py

--- data_reader.py
+++ data_reader.py
@@ -3,5 +3,5 @@ import numpy as np
 import pdb
 
 dataset = pd.read_csv("../NYPD_Complaint_Data_Historic.csv", low_memory=False)
-dataset.drop(columns = ['CMPLNT_NUM', 'PD_CD', 'KY_CD', 'HOUSING_PSA', 'PARKS_NM', 'TRANSIT_DISTRICT', 'STATION_NAME'], inplace = True)
+dataset.drop(columns = ['CMPLNT_NUM', 'PD_CD', 'KY_CD', 'HOUSING_PSA', 'PARKS_NM', 'TRANSIT_DISTRICT', 'STATION_NAME' ,'HADEVELOPT'], inplace = True)
 pdb.set_trace()
