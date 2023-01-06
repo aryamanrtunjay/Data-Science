256b35f0cb0a8ab443d98838a77ce85392d84741
Aryaman R <aryamanrtunjay@gmail.com>
2023-01-05 19:45:29 -0800

    Added a time graph

--- data_reader.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-import pdb
-
-dataset = pd.read_csv("../NYPD_Complaint_Data_Historic.csv", low_memory=False)
-dataset.drop(columns = ['CMPLNT_NUM', 'PD_CD', 'KY_CD', 'HOUSING_PSA', 'PARKS_NM', 'TRANSIT_DISTRICT', 'STATION_NAME' ,'HADEVELOPT'], inplace = True)
-pdb.set_trace()
