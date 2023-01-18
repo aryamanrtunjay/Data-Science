c9a8ebc695a19bd29d134a1e6f13c8b96629dfd6
Raghav Kalyan <raghavkalyan25@outlook.com>
2023-01-17 21:20:22 -0800

    commit

--- all_graphs.py
+++ all_graphs.py
@@ -2,8 +2,10 @@ import vic_race
 import crime_pychart
 import time_graph
 import day_graph
+import premise_description_graph
 
 vic_race.get_graph()
 crime_pychart.get_graph()
 time_graph.get_graph()
 day_graph.get_graph()
+premise_description_graph.get_graph()
