cae3641d8e0ec77d659c11cdc6726ca2d1129eb4
Raghav Kalyan <raghavkalyan25@outlook.com>
2023-03-08 17:19:38 -0800

    updated accuracy predictions

--- Models/datamodel.py
+++ Models/datamodel.py
@@ -1,24 +1,26 @@
 import tensorflow as tf
-from tensorflow import keras
+import keras
 from keras import backend as K
 from keras import layers
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 import pandas as pd
+import os
 import pdb
 import wandb
 
 # Read the dataset from a pickle file if it exists, else read from the csv file and create the pickle file
+datapath = "/home/raghavkalyan/Files/Documents/Devolopment/TSA/Data-Science/NYPD_Complaint_Data_Historic.csv"
 
-if(os.path.exists("../X_total.pickle") and os.path.exist("../Y_total.pickle")):
-    X_total = pd.read_pickle("../X_total.pickle")
-    Y_total = pd.read_pickle("../Y_total.pickle")
+if(os.path.exists("./X_total.pickle") and os.path.exist("./Y_total.pickle")):
+    X_total = pd.read_pickle("./X_total.pickle")
+    Y_total = pd.read_pickle("./Y_total.pickle")
 else:
     if os.path.exists('../data.pickle'):
-        dataset = pd.read_pickle('../data.pickle')
+        dataset = pd.read_pickle('./data.pickle')
     else:
-        dataset = pd.read_csv("../NYPD_Complaint_Data_Historic.csv", low_memory=False)
-        dataset.to_pickle('../data.pickle')
+        dataset = pd.read_csv("./NYPD_Complaint_Data_Historic.csv", low_memory=False)
+        dataset.to_pickle('./data.pickle')
     # Get train and test datasets
     subset = dataset[['CMPLNT_FR_TM','ADDR_PCT_CD', 'LAW_CAT_CD', 'SUSP_RACE']].copy(deep=True)
     subset.dropna(axis = 0, inplace=True)
@@ -38,7 +40,7 @@ else:
     le = LabelEncoder()
     Y_total = le.fit_transform(Y_total)
     Y_total = pd.DataFrame(Y_total)
-
+    
     X_total['CMPLNT_FR_TM'] = X_total['CMPLNT_FR_TM'].str.split(':').str[0]
     X_total['CMPLNT_FR_TM'] = X_total['CMPLNT_FR_TM'].astype(int)
     pd.to_pickle(X_total, "../X_total.pickle")
@@ -48,16 +50,16 @@ X_train, X_test, Y_train, Y_test = train_test_split(X_total, Y_total, test_size=
 
 # wandb.init(project="nypd-crime", name="NYPD-Crime-ANN")
 model = keras.Sequential([
-    keras.layers.Dense(8, input_shape=(3,), activation='relu'),
-    keras.layers.Dense(16, activation='relu'),
-    keras.layers.Dense(16, activation='relu'),
-    keras.layers.Dense(8, activation='softmax')
+    keras.layers.Dense(32, input_shape=(3,), activation='relu'),
+    keras.layers.Dense(64, activation='relu'),
+    keras.layers.Dense(64, activation='relu'),
+    keras.layers.Dense(32, activation='softmax')
 ])
 
-model.compile(optimizer='adam',
+model.compile(optimizer='AdaGrad',
               loss='sparse_categorical_crossentropy',
               metrics=['Accuracy'])
 K.set_value(model.optimizer.learning_rate, 0.1)
 
-model.fit(X_train, Y_train, epochs=200, batch_size=256)
+model.fit(X_train, Y_train, epochs=400, batch_size=100)
 model.evaluate(X_test, Y_test)
